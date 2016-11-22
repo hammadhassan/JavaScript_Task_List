acfc61fca71e3045de28651220ddbeb35856e132
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-23 01:26:07 +0500

    initialize repo

---
+++ app.js
@@ -0,0 +1,7 @@
+var list = [];
+for(var i = 0; i < list.length; i++) {
+    document.getElementsByTagName("li")= this.innerHTML;
+} 
+function add() {
+    document.getElementById("list") = this.innerHTML;
+}
