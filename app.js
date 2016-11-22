bd10875e233e83604d1fde12c7657ca8a4d7a788
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-23 01:42:15 +0500

    try to add list

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-var list = [];
-for(var i = 0; i < list.length; i++) {
+var tasklist = [];
+/*for(var i = 0; i < list.length; i++) {
     document.getElementsByTagName("li")= this.innerHTML;
-} 
+} */
 function add() {
-    document.getElementById("list") = this.innerHTML;
+    //document.getElementById("list") = this.innerHTML;
+    var GetInputValue = document.getElementById("input").value;
+    if (GetInputValue == " " || GetInputValue == "") {
+        alert("Please write some thing");
+    }
+    else {
+        tasklist.push(GetInputValue);
+    }
+    if (tasklist.length >= 1) {
+        document.getElementById("heading").innerHTML = "Enter Your Task";
+    }
+    var List = document.getElementById("list");
+    var data = "";
+    for (var i = 0; i < tasklist.length; i++) {
+        data += "<li>" + tasklist[i] + "</li>";
+        List.innerHTML = data;
+    }
+    data = "";
+    document.getElementById("myInput").value = "";
+
+}
+function removed() {
+    list = [];
 }
