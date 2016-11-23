10ed87afd01d5d9da8b501c74f389e34b5f92bf1
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-24 01:19:28 +0500

    add button with list

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var tasklist = [];
-/*for(var i = 0; i < list.length; i++) {
-    document.getElementsByTagName("li")= this.innerHTML;
-} */
 function add() {
+    if (tasklist.length = 1) {
+        tasklist.push(document.getElementById("list").innerHTML = ("<li>" + tasklist[i] + "sad"); 
+    }
+    document.getElementById("list").innerHTML=("<li>" + tasklist[i] + "</li>");
+    var info = "";
+    for(var i = 0; i <= tasklist.length; i++) {
+        info += document.getElementById("takslist").innerHTML = ("<li>" + tasklist[i] + "<li>");
+    }
+}
+
+/*function add() {
     //document.getElementById("list") = this.innerHTML;
     var GetInputValue = document.getElementById("input").value;
     if (GetInputValue == " " || GetInputValue == "") {
@@ -23,7 +31,8 @@ function add() {
     data = "";
     document.getElementById("myInput").value = "";
 
-}
+}*/
 function removed() {
-    list = [];
+    tasklist = [];
+    list = "";
 }
