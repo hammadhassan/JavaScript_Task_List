dc93bec7e489db78c6c03fa46d91ae64aca99b5f
hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-25 16:52:37 +0500

    try to add

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
 var tasklist = [];
 function add() {
     if (tasklist.length = 1) {
-        tasklist.push(document.getElementById("list").innerHTML = ("<li>" + tasklist[i] + "sad"); 
+        var name = document.getElementById("myInput").innerHTML = ("<li>" + tasklist[i] + "</li>");
+        //tasklist.push(document.getElementById("list").innerHTML = ("<li>" + tasklist[i] + "</li>"); 
     }
-    document.getElementById("list").innerHTML=("<li>" + tasklist[i] + "</li>");
+    //document.getElementById("list").innerHTML=("<li>" + tasklist[i] + "</li>");
     var info = "";
     for(var i = 0; i <= tasklist.length; i++) {
-        info += document.getElementById("takslist").innerHTML = ("<li>" + tasklist[i] + "<li>");
+        info += document.getElementById("myInput").innerHTML = ("<li>" + tasklist[i] + "<li>");
     }
 }
-
-/*function add() {
-    //document.getElementById("list") = this.innerHTML;
-    var GetInputValue = document.getElementById("input").value;
-    if (GetInputValue == " " || GetInputValue == "") {
-        alert("Please write some thing");
-    }
-    else {
-        tasklist.push(GetInputValue);
-    }
-    if (tasklist.length >= 1) {
-        document.getElementById("heading").innerHTML = "Enter Your Task";
-    }
-    var List = document.getElementById("list");
-    var data = "";
-    for (var i = 0; i < tasklist.length; i++) {
-        data += "<li>" + tasklist[i] + "</li>";
-        List.innerHTML = data;
-    }
-    data = "";
-    document.getElementById("myInput").value = "";
-
-}*/
 function removed() {
     tasklist = [];
     list = "";
