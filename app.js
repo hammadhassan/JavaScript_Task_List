aee446e8b854a32171e820f2edda8c76c102db0a
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-12-02 01:01:34 +0500

    apply edit button

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function add() {
     var list = document.getElementById("list");
     var info = "";
     for(var i = 0; i < tasklist.length; i++) {
-        info = info + ("<li>" + tasklist[i] + "<button type='edit' class='edit' >edit</button>" + "<button class='delete' type='delet'>Delete</button>" +"</li>"); 
+        info = info + ("<li>" + tasklist[i] + "<button type='edit' class='edit' onClick='edit();'>edit</button>" + "<button class='delete' type='delet' onClick='deleteb();'>Delete</button>" +"</li>"); 
         list.innerHTML = info;
     }
     myInput.value = "";
@@ -26,11 +26,11 @@ function removed() {
     document.getElementById("list").style.display = "none";
 }
 function edit() {
-    //document.getElementsByTagName("li").innerHTML = ("<input type = 'edit' value = 'edit' onClick = 'edit()' >" + tasklist )
-    alert("yes");
-    
+    var tasklist1 = prompt("edit");
+    tasklist1.push(info);
+    //alert("yes");
 }
-if (tasklist.length <= 1) {
-        document.getElementsByTagName("li").innerHTML = ("<button type = 'edit' value = 'edit' >" + "edit" + "</button>")
-    }
-    //alert(edit);
+function deleteb() {
+    //document.getElementsByTagName("li").style.display = "none";
+    alert("work");
+}
